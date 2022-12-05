43c36bd9849f6438ff1a4c9188b0fe8e97c6da60
Anton Giertli <agiertli@redhat.com>
2022-12-05 14:33:53 +0100

    example create-secret.sh

--- charts/create-secrets.sh
+++ charts/create-secrets.sh
@@ -1,7 +1,6 @@
 oc process -f artemis-secret.yaml \
--p USER_NAMESPACE=<TODO> \
--p USERNAME=<TODO> \
--p PASSWORD=<TODO> \
--p TRUSTSTORE_PASSWORD=<TODO> \
--p BROKER_URL=<TODO> \
+-p USERNAME=admin \
+-p PASSWORD=password1! \
+-p TRUSTSTORE_PASSWORD=password1! \
+-p BROKER_URL=amqps://rhte-artemis-one-way-ssl-0-svc.tooling.svc.cluster.local:5673 \
 | oc apply -f -
