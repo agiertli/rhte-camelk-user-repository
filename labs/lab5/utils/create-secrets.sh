92f60dd3eccac13d4902a1322d8b5644a7ea7a9b
Anton Giertli <agiertli@redhat.com>
2022-12-23 17:05:41 +0100

    add client.ts

--- labs/lab5/utils/create-secrets.sh
+++ labs/lab5/utils/create-secrets.sh
@@ -5,7 +5,7 @@ oc process -f artemis-secret.yaml \
 -p PASSWORD=password1! \
 -p TRUSTSTORE_PASSWORD=password1! \
 -p BROKER_URL=amqps://rhte-artemis-one-way-ssl-0-svc.tooling.svc.cluster.local:5673 \
-| oc apply -f - -o yaml --dry-run=client | kubeseal --controller-namespace tooling --controller-name sealed-secrets-controller --format yaml > sealed-amq-secret.yaml
+| oc create -f - -o yaml --dry-run=client | kubeseal --controller-namespace tooling --controller-name sealed-secrets-controller --format yaml > sealed-amq-secret.yaml
 
 echo "Creating truststore secret"
  oc create secret generic truststore-secret --from-file=client.ts -o yaml --dry-run=client | kubeseal --controller-namespace tooling --controller-name sealed-secrets-controller --format yaml > truststore-secret.yaml
